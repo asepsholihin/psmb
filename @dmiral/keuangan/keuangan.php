a536405d5f2a4618cb9ca7ce1d84b3eb4ed6526f
Asep Sholihin <asep.sholihin11@gmail.com>
2017-03-20 11:28:57 +0700

    fix bug

--- @dmiral/keuangan/keuangan.php
+++ @dmiral/keuangan/keuangan.php
@@ -12,86 +12,93 @@ if($_POST['transfer'] == 1) {
   $transfer = 0;
 }
 
+// membuat no referensi
+$result = mysql_query("SELECT substring(id_referensi,5,7) as id FROM log_transaksi ORDER BY id_referensi DESC LIMIT 1");
+$row = mysql_fetch_assoc($result);
+// menambah nilai id
+$upid = $row['id'] + 1;
+$lastid = date('Y').sprintf('%06s', $upid);
+
 if($_POST['psp'] != "") {
-    $sql1 = "INSERT INTO log_transaksi VALUES ('".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['psp'])."','Pengembangan Sarana & Prasarana','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
+    $sql1 = "INSERT INTO log_transaksi VALUES ('','".$lastid."','".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['psp'])."','Pengembangan Sarana & Prasarana','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
     mysql_query($sql1);
     $response["error"] = "false";
 }
 
 if($_POST['psdm'] != "") {
-    $sql2 = "INSERT INTO log_transaksi VALUES ('".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['psdm'])."','Pengembangan SDM','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
+    $sql2 = "INSERT INTO log_transaksi VALUES ('','".$lastid."','".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['psdm'])."','Pengembangan SDM','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
     mysql_query($sql2);
     $response["error"] = "false";
 }
 
 if($_POST['kgttahunan'] != "") {
-    $sql3 = "INSERT INTO log_transaksi VALUES ('".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['kgttahunan'])."','Kegiatan Tahunan','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
+    $sql3 = "INSERT INTO log_transaksi VALUES ('','".$lastid."','".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['kgttahunan'])."','Kegiatan Tahunan','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
     mysql_query($sql3);
     $response["error"] = "false";
 }
 
 if($_POST['seragam'] != "") {
-    $sql4 = "INSERT INTO log_transaksi VALUES ('".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['seragam'])."','Seragam','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
+    $sql4 = "INSERT INTO log_transaksi VALUES ('','".$lastid."','".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['seragam'])."','Seragam','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
     mysql_query($sql4);
     $response["error"] = "false";
 }
 
 if($_POST['buku'] != "") {
-    $sql5 = "INSERT INTO log_transaksi VALUES ('".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['buku'])."','Buku Paket & Modul','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
+    $sql5 = "INSERT INTO log_transaksi VALUES ('','".$lastid."','".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['buku'])."','Buku Paket & Modul','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
     mysql_query($sql5);
     $response["error"] = "false";
 }
 
 if($_POST['orientasi'] != "") {
-    $sql6 = "INSERT INTO log_transaksi VALUES ('".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['orientasi'])."','Orientasi Dan I\'dad','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
+    $sql6 = "INSERT INTO log_transaksi VALUES ('','".$lastid."','".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['orientasi'])."','Orientasi Dan I\'dad','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
     mysql_query($sql6);
     $response["error"] = "false";
 }
 
 if($_POST['spp'] != "") {
-    $sql7 = "INSERT INTO log_transaksi VALUES ('".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['spp'])."','Iuran Bulanan/SPP','".$_POST['keterangan']." ".$_POST['keteranganthn']."','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
+    $sql7 = "INSERT INTO log_transaksi VALUES ('','".$lastid."','".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['spp'])."','Iuran Bulanan/SPP','".$_POST['keterangan']." ".$_POST['keteranganthn']."','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
     mysql_query($sql7);
     $response["error"] = "false";
 }
 
 if($_POST['komitmenbulanan'] != "") {
-    $sql8 = "INSERT INTO log_transaksi VALUES ('".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['komitmenbulanan'])."','Komitmen Bulanan','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
+    $sql8 = "INSERT INTO log_transaksi VALUES ('','".$lastid."','".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['komitmenbulanan'])."','Komitmen Bulanan','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
     mysql_query($sql8);
     $response["error"] = "false";
 }
 
 if($_POST['komitmentahunan'] != "") {
-    $sql8 = "INSERT INTO log_transaksi VALUES ('".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['komitmentahunan'])."','Komitmen Tahunan','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
+    $sql8 = "INSERT INTO log_transaksi VALUES ('','".$lastid."','".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['komitmentahunan'])."','Komitmen Tahunan','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
     mysql_query($sql8);
     $response["error"] = "false";
 }
 
 if($_POST['uangsaku'] != "") {
-    $sql9 = "INSERT INTO log_transaksi VALUES ('".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['uangsaku'])."','Uang Saku','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
+    $sql9 = "INSERT INTO log_transaksi VALUES ('','".$lastid."','".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['uangsaku'])."','Uang Saku','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
     mysql_query($sql9);
     $response["error"] = "false";
 }
 
 if($_POST['kaskomite'] != "") {
-    $sql10 = "INSERT INTO log_transaksi VALUES ('".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['kaskomite'])."','Kas Komite','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
+    $sql10 = "INSERT INTO log_transaksi VALUES ('','".$lastid."','".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['kaskomite'])."','Kas Komite','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
     mysql_query($sql10);
     $response["error"] = "false";
 }
 
 if($_POST['infaq'] != "") {
-    $sql11 = "INSERT INTO log_transaksi VALUES ('".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['infaq'])."','Infaq','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
+    $sql11 = "INSERT INTO log_transaksi VALUES ('','".$lastid."','".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['infaq'])."','Infaq','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
     mysql_query($sql11);
     $response["error"] = "false";
 }
 
 if($_POST['qurban'] != "") {
-    $sql12 = "INSERT INTO log_transaksi VALUES ('".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['qurban'])."','Qurban','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
+    $sql12 = "INSERT INTO log_transaksi VALUES ('','".$lastid."','".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['qurban'])."','Qurban','','',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
     mysql_query($sql12);
     $response["error"] = "false";
 }
 
 if($_POST['lain'] != "") {
-    $sql13 = "INSERT INTO log_transaksi VALUES ('".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['lain'])."','Lain-lain','','".$_POST['catatan']."',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
+    $sql13 = "INSERT INTO log_transaksi VALUES ('','".$lastid."','".addslashes($_POST['nopendaftaran'])."','".str_replace(',','',$_POST['lain'])."','Lain-lain','','".$_POST['catatan']."',NOW(),'".$_POST['tanggal_transaksi']."','".$transfer."','Evi Siti Soviani','0')";
     mysql_query($sql13);
     $response["error"] = "false";
 }
--- @dmiral/pages/rekap-daftarulang.php
+++ @dmiral/pages/rekap-daftarulang.php
@@ -1,25 +1,23 @@
 <?php
 include "config.php";
 $data = "";
-$sql = "SELECT a.nopendaftaran, a.nama, b.tanggal_transaksi FROM calonsiswa a LEFT JOIN log_transaksi b ON a.nopendaftaran=b.nopendaftaran WHERE lulus=1 OR lulus=2 GROUP BY a.nopendaftaran, b.tanggal_transaksi ORDER BY nama ASC";
+$sql = "SELECT b.id_referensi, a.nopendaftaran, a.nama, b.tanggal_transaksi FROM calonsiswa a LEFT JOIN log_transaksi b ON a.nopendaftaran=b.nopendaftaran WHERE lulus=1 OR lulus=2 GROUP BY a.nopendaftaran, b.tanggal_transaksi, b.transfer ORDER BY nama ASC";
 $query = mysql_query($sql);
 $no = 1;
 while($row = mysql_fetch_assoc($query))
 {
     $sqls = "
     SELECT b.nama, a.transfer, a.tanggal_transaksi,
-    (SELECT jumlah FROM log_transaksi WHERE nopendaftaran='".$row['nopendaftaran']."' AND jenis='Pengembangan Sarana & Prasarana' AND tanggal_transaksi='".$row['tanggal_transaksi']."' GROUP BY nopendaftaran) AS 'psp',
-    (SELECT jumlah FROM log_transaksi WHERE nopendaftaran='".$row['nopendaftaran']."' AND jenis='Pengembangan SDM' AND tanggal_transaksi='".$row['tanggal_transaksi']."' GROUP BY nopendaftaran) AS 'psdm',
-    (SELECT jumlah FROM log_transaksi WHERE nopendaftaran='".$row['nopendaftaran']."' AND jenis='Kegiatan Tahunan' AND tanggal_transaksi='".$row['tanggal_transaksi']."' GROUP BY nopendaftaran) AS 'kgttahunan',
-    (SELECT jumlah FROM log_transaksi WHERE nopendaftaran='".$row['nopendaftaran']."' AND jenis='Seragam' AND tanggal_transaksi='".$row['tanggal_transaksi']."' GROUP BY nopendaftaran) AS 'seragam',
-    (SELECT jumlah FROM log_transaksi WHERE nopendaftaran='".$row['nopendaftaran']."' AND jenis='Buku Paket & Modul' AND tanggal_transaksi='".$row['tanggal_transaksi']."' GROUP BY nopendaftaran) AS 'buku',
-    (SELECT jumlah FROM log_transaksi WHERE nopendaftaran='".$row['nopendaftaran']."' AND jenis='Orientasi Dan I\'dad' AND tanggal_transaksi='".$row['tanggal_transaksi']."' GROUP BY nopendaftaran) AS 'orientasi'
+    (SELECT jumlah FROM log_transaksi WHERE nopendaftaran='".$row['nopendaftaran']."' AND jenis='Pengembangan Sarana & Prasarana' AND id_referensi='".$row['id_referensi']."' GROUP BY nopendaftaran) AS 'psp',
+    (SELECT jumlah FROM log_transaksi WHERE nopendaftaran='".$row['nopendaftaran']."' AND jenis='Pengembangan SDM' AND id_referensi='".$row['id_referensi']."' GROUP BY nopendaftaran) AS 'psdm',
+    (SELECT jumlah FROM log_transaksi WHERE nopendaftaran='".$row['nopendaftaran']."' AND jenis='Kegiatan Tahunan' AND id_referensi='".$row['id_referensi']."' GROUP BY nopendaftaran) AS 'kgttahunan',
+    (SELECT jumlah FROM log_transaksi WHERE nopendaftaran='".$row['nopendaftaran']."' AND jenis='Seragam' AND id_referensi='".$row['id_referensi']."' GROUP BY nopendaftaran) AS 'seragam',
+    (SELECT jumlah FROM log_transaksi WHERE nopendaftaran='".$row['nopendaftaran']."' AND jenis='Buku Paket & Modul' AND id_referensi='".$row['id_referensi']."' GROUP BY nopendaftaran) AS 'buku',
+    (SELECT jumlah FROM log_transaksi WHERE nopendaftaran='".$row['nopendaftaran']."' AND jenis='Orientasi Dan I\'dad' AND id_referensi='".$row['id_referensi']."' GROUP BY nopendaftaran) AS 'orientasi'
     FROM log_transaksi a JOIN calonsiswa b ON a.nopendaftaran=b.nopendaftaran WHERE a.nopendaftaran='".$row['nopendaftaran']."' GROUP BY a.nopendaftaran
     ";
     $querys = mysql_query($sqls);
     $uang = mysql_fetch_assoc($querys);
-
-
 
     if($uang['nama'] != '') {
       if($$uang['transfer'] == 1) {
@@ -50,6 +48,25 @@ while($row = mysql_fetch_assoc($query))
     $date = date_create($row['tgl_konfirmasi']);
 }
 
+$sqltotal = "
+SELECT
+(SELECT IFNULL(SUM(jumlah), 0) FROM `log_transaksi` WHERE jenis='Orientasi Dan I\'dad') AS orientasi,
+(SELECT IFNULL(SUM(jumlah), 0) FROM `log_transaksi` WHERE jenis='Buku Paket & Modul') AS buku,
+(SELECT IFNULL(SUM(jumlah), 0) FROM `log_transaksi` WHERE jenis='Seragam') AS seragam,
+(SELECT IFNULL(SUM(jumlah), 0) FROM `log_transaksi` WHERE jenis='Kegiatan Tahunan') AS kgttahunan,
+(SELECT IFNULL(SUM(jumlah), 0) FROM `log_transaksi` WHERE jenis='Pengembangan SDM') AS psdm,
+(SELECT IFNULL(SUM(jumlah), 0) FROM `log_transaksi` WHERE jenis='Pengembangan Sarana & Prasarana') AS psp,
+(SELECT IFNULL(SUM(jumlah), 0) FROM `log_transaksi` WHERE jenis='Komitmen Bulanan') AS komitmenbulanan,
+(SELECT IFNULL(SUM(jumlah), 0) FROM `log_transaksi` WHERE jenis='Komitmen Tahunan') AS komitmentahunan,
+(SELECT IFNULL(SUM(jumlah), 0) FROM `log_transaksi` WHERE jenis='Uang Saku') AS uangsaku,
+(SELECT IFNULL(SUM(jumlah), 0) FROM `log_transaksi` WHERE jenis='Kas Komite') AS kaskomite,
+(SELECT IFNULL(SUM(jumlah), 0) FROM `log_transaksi` WHERE jenis='Infaq') AS infaq,
+(SELECT IFNULL(SUM(jumlah), 0) FROM `log_transaksi` WHERE jenis='Qurban') AS qurban,
+(SELECT IFNULL(SUM(jumlah), 0) FROM `log_transaksi` WHERE jenis='Iuran Bulanan/SPP') AS spp
+";
+$querytotal = mysql_query($sqltotal);
+$total = mysql_fetch_array($querytotal);
+
 $content = "
 $kwitansi
 <link rel=\"stylesheet\" type=\"text/css\" href=\"//cdn.datatables.net/1.10.12/css/jquery.dataTables.css\">
@@ -57,13 +74,13 @@ $kwitansi
 	<thead>
 		<tr>
       <th class=\"no-sort\" width=\"1\">No</th>
-			<th>Nama</th>
-			<th width=\"1\" class=\"no-sort center\">ORIENTASI DAN I'DAD</th>
-			<th width=\"1\" class=\"no-sort center\">BUKU PAKET DAN MODUL</th>
-			<th width=\"1\" class=\"no-sort center\">SERAGAM</th>
-			<th width=\"1\" class=\"no-sort center\">KEGIATAN TAHUNAN</th>
-			<th width=\"1\" class=\"no-sort center\">PENGEMBANGAN SDM</th>
-			<th width=\"1\" class=\"no-sort center\">PENGEMBANGAN SARPRAS</th>
+			<th width=\"1\">Nama</th>
+			<th class=\"no-sort center\">ORIENTASI DAN I'DAD</th>
+			<th class=\"no-sort center\">BUKU PAKET DAN MODUL</th>
+			<th class=\"no-sort center\">SERAGAM</th>
+			<th class=\"no-sort center\">KEGIATAN TAHUNAN</th>
+			<th class=\"no-sort center\">PENGEMBANGAN SDM</th>
+			<th class=\"no-sort center\">PENGEMBANGAN SARPRAS</th>
 			<th width=\"1\" class=\"no-sort center\">Tanggal</th>
 			<th width=\"1\" class=\"center\">Transaksi</th>
 		</tr>
@@ -72,6 +89,63 @@ $kwitansi
 		$data
 	</tbody>
 </table>
+<br><br>
+
+<div class=\"wrapbox\">
+  <div class=\"countbox\">
+      <h2>Rp. ".number_format($total['orientasi'],0,'.','.')."</h2></a>
+      Orientasi Dan I'dad
+  </div>
+  <div class=\"countbox\">
+      <h2>Rp. ".number_format($total['buku'],0,'.','.')."</h2></a>
+      Buku Paket & Modul
+  </div>
+  <div class=\"countbox\">
+      <h2>Rp. ".number_format($total['seragam'],0,'.','.')."</h2></a>
+      Seragam
+  </div>
+  <div class=\"countbox\">
+      <h2>Rp. ".number_format($total['kgttahunan'],0,'.','.')."</h2></a>
+      Kegiatan Tahunan
+  </div>
+</div>
+<div class=\"wrapbox\">
+  <div class=\"countbox\">
+      <h2>Rp. ".number_format($total['psdm'],0,'.','.')."</h2></a>
+      Pengembangan SDM
+  </div>
+  <div class=\"countbox\">
+      <h2>Rp. ".number_format($total['psp'],0,'.','.')."</h2></a>
+      Pengembangan Sarpras
+  </div>
+  <div class=\"countbox\">
+      <h2>Rp. ".number_format($total['komitmenbulanan'],0,'.','.')."</h2></a>
+      Komitmen Bulanan
+  </div>
+  <div class=\"countbox\">
+      <h2>Rp. ".number_format($total['komitmentahunan'],0,'.','.')."</h2></a>
+      Komitmen Tahunan
+  </div>
+</div>
+
+<div class=\"wrapbox\">
+  <div class=\"countbox\">
+      <h2>Rp. ".number_format($total['uangsaku'],0,'.','.')."</h2></a>
+      Uang Saku
+  </div>
+  <div class=\"countbox\">
+      <h2>Rp. ".number_format($total['kaskomite'],0,'.','.')."</h2></a>
+      Kas Komite
+  </div>
+  <div class=\"countbox\">
+      <h2>Rp. ".number_format($total['infaq'],0,'.','.')."</h2></a>
+      Infaq
+  </div>
+  <div class=\"countbox\">
+      <h2>Rp. ".number_format($total['qurban'],0,'.','.')."</h2></a>
+      Qurban
+  </div>
+</div>
 
 <script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js\"></script>
 <script type=\"text/javascript\" charset=\"utf8\" src=\"//cdn.datatables.net/1.10.12/js/jquery.dataTables.js\"></script>
@@ -85,7 +159,7 @@ $(document).ready(function(){
             }
         ]
     });
-    $('.calonsantri').addClass('active');
+    $('.konfirmasipembayaran').addClass('active');
 
 });
 </script>
